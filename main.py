23cf84e5a0c1bab488acef4d143aef0ba0039ccc
nkybahibey <odiinkechinyere@gmail.com>
2023-04-20 18:21:06 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+print("Welcome to Band name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your band name could be " + "Igoli" + " " + "Masarati")
